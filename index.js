e607550c66f6edd81d1c99c5616711dfb630ea21
Gardem <dmitryarchebasov@gmail.com>
2018-03-02 15:14:59 +0300

    fs

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-exports.inject = function (spriteDir) {
-    console.log('do inject this from', spriteDir);
+const fs = require('fs');
+
+exports.run = function (spriteFilePath, outputFilePath) {
+    // todo inject file into html
+    const spriteSrc = spriteFilePath
+    const outputSrc = outputFilePath
+    let buffer = null
+    fs.readFile(spriteSrc, 'utf8', function (err, data) {
+        if (err) throw err
+        fs.open(outputSrc, 'w', function (err, fd) {
+            if (err) { throw 'error opening file: ' + err }
+            const resultStr = '<!DOCTYPE html>\n' +
+                '<html {{ HTML_ATTRS }}>\n' +
+                '  <head>\n' +
+                '    {{ HEAD }}\n' +
+                '  </head>\n' +
+                '  <body {{ BODY_ATTRS }}>\n' +
+                data +
+                '    {{ APP }}\n' +
+                '  </body>\n' +
+                '</html>'
+            buffer = new Buffer(resultStr)
+            fs.write(fd, buffer, 0, buffer.length, null, function (err) {
+                if (err) throw 'error writing file: ' + err
+                fs.close(fd, function () {
+                    console.log('sprite has been appended to body')
+                })
+            })
+        })
+    })
 }
